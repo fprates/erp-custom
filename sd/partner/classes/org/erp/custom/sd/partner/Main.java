c44c99fe6e4de7dc75fd4242aa5e841071c62349
Francisco Prates <mad.kanie@gmail.com>
2012-02-15 01:33:35 -0200

    partner: implementação inicial de header da visão identity.
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- sd/partner/classes/org/erp/custom/sd/partner/Main.java
+++ sd/partner/classes/org/erp/custom/sd/partner/Main.java
@@ -1,5 +1,7 @@
 package org.erp.custom.sd.partner;
 
+import org.iocaste.documents.common.DocumentModel;
+import org.iocaste.documents.common.DocumentModelItem;
 import org.iocaste.shell.common.AbstractPage;
 import org.iocaste.shell.common.Button;
 import org.iocaste.shell.common.Const;
@@ -20,6 +22,16 @@ public class Main extends AbstractPage {
             "partner-update"
     };
     
+    private final void addModelItem(DocumentModel model, String name) {
+        DocumentModelItem item = new DocumentModelItem();
+        
+        item.setName(name);
+        item.setDocumentModel(model);
+        item.setIndex(model.getItens().size());
+        
+        model.add(item);
+    }
+    
     public void create(ViewData view) {
         
     }
@@ -27,6 +39,19 @@ public class Main extends AbstractPage {
     public void identity(ViewData view) {
         Container container = new Form(null, "main");
         byte modo = (Byte)view.getParameter("mode");
+        DataForm header = new DataForm(container, "header");
+        DocumentModel model = new DocumentModel();
+        
+        addModelItem(model, "codigo");
+        addModelItem(model, "nomeRazao");
+        addModelItem(model, "nomeFantasia");
+        addModelItem(model, "docFiscal");
+        addModelItem(model, "insEstadual");
+        addModelItem(model, "insMunicipal");
+        addModelItem(model, "pessoaFJ");
+        addModelItem(model, "codigo");
+        
+        header.importModel(model);
         
         view.setNavbarActionEnabled("back", true);
         view.setTitle(TITLE[modo]);
