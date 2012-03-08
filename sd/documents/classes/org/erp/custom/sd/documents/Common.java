8120f7b8fe746773041e9f502908f760002d575d
Francisco Prates <mad.kanie@gmail.com>
2012-03-08 00:51:56 -0300

    documents: implementa adição de item.
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- sd/documents/classes/org/erp/custom/sd/documents/Common.java
+++ sd/documents/classes/org/erp/custom/sd/documents/Common.java
@@ -1,5 +1,6 @@
 package org.erp.custom.sd.documents;
 
+import org.iocaste.shell.common.InputComponent;
 import org.iocaste.shell.common.Table;
 import org.iocaste.shell.common.TableColumn;
 import org.iocaste.shell.common.TableItem;
@@ -22,14 +23,30 @@ public class Common {
     }
     
     public static final void insertItem(Table itens) {
+        String name;
         TextField tfield;
         TableItem item = new TableItem(itens);
+        int i = 0;
         
-        for (TableColumn column : itens.getColumns())
-            if (!column.isMark()) {
-                tfield = new TextField(itens, column.getName());
-                tfield.setModelItem(column.getModelItem());
-                item.add(tfield);
-            }
+        for (TableItem item_ : itens.getItens())
+            i = Integer.parseInt(
+                    ((InputComponent)item_.get("ITEM_NUMBER")).getValue());
+        
+        item = new TableItem(itens);
+        i++;
+        
+        for (TableColumn column : itens.getColumns()) {
+            if (column.isMark())
+                continue;
+            
+            name = column.getName();
+            
+            tfield = new TextField(itens, name);
+            tfield.setModelItem(column.getModelItem());
+            item.add(tfield);
+            
+            if (column.getName().equals("ITEM_NUMBER"))
+                tfield.setValue(Integer.toString(i));
+        }
     }
 }
