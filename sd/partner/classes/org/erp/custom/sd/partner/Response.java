d45fba42f997c0953f7d5d1ecec38e099a8b914b
Francisco Prates <mad.kanie@gmail.com>
2012-05-15 23:30:52 -0300

    shell: reimplementaçao de dados basicos do parceiro.
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- sd/partner/classes/org/erp/custom/sd/partner/Response.java
+++ sd/partner/classes/org/erp/custom/sd/partner/Response.java
@@ -19,33 +19,206 @@ import org.iocaste.shell.common.ViewData;
 
 public class Response {
 
-    /**
-     * 
-     * @param view
-     * @param identitymodel
-     * @param addressmodel
-     */
-    public static final void identity(ViewData view, DocumentModel[] models) {
+    private static final void buildAddressTab(TabbedPane tabs,
+            DocumentModel model, ViewData view) {
         ItemData itemdata;
-        String name;
-        DataItem dataitem;
-        DataForm partner, address, contact;
-        Table contacts, addresses;
         TabbedPaneItem tab;
-        Button save, editaddress, addaddress, removeaddress, addcontact,
-                removecontact, editcontact;
-        Container contactcnt, addresscnt, container = new Form(view, "main");
-        TabbedPane tabs = new TabbedPane(container, "pane");
+        Table addresses;
+        Button editaddress, addaddress, removeaddress;
+        Container addresscnt = new StandardContainer(tabs, "addresscnt");
+        DataForm address = new DataForm(addresscnt, "address");
         byte modo = Common.getMode(view);
-        ExtendedObject opartner = view.getParameter("partner");
         ExtendedObject[] oaddresses = view.getParameter("addresses");
+        
+        address.importModel(model);
+        address.get("CODIGO").setEnabled(false);
+        address.get("PARTNER_ID").setVisible(false);
+        
+        editaddress = new Button(addresscnt, "editaddress");
+        addaddress = new Button(addresscnt, "addaddress");
+        removeaddress = new Button(addresscnt, "removeaddress");
+        
+        addresses = new Table(addresscnt, "addresses");
+        addresses.importModel(model);
+        addresses.setVisible(false);
+        addresses.setMark(true);
+        
+        Common.enableTableColumns(addresses,
+                "CODIGO", "TIPO_ENDERECO", "LOGRADOURO");
+        
+        tab = new TabbedPaneItem(tabs, "addresstab");
+        tab.setContainer(addresscnt);
+        
+        switch (modo) {
+        case Common.CREATE:
+            editaddress.setVisible(false);
+            removeaddress.setVisible(false);
+            
+            break;
+        
+        case Common.SHOW:
+            addresses.setMark(false);
+
+            if (oaddresses != null) {
+                itemdata = new ItemData();
+                itemdata.container = addresscnt;
+                itemdata.itens = addresses;
+                itemdata.mark = "addressmark";
+                addresses.setVisible(true);
+                
+                for (ExtendedObject oaddress : oaddresses) {
+                    itemdata.object = oaddress;
+                    Common.insertItem(itemdata);
+                }
+            }
+            
+            editaddress.setVisible(false);
+            addaddress.setVisible(false);
+            removeaddress.setVisible(false);
+            
+            break;
+            
+        case Common.UPDATE:
+            if (oaddresses == null)
+                break;
+            
+            addresses.setMark(true);
+            itemdata = new ItemData();
+            itemdata.container = addresscnt;
+            itemdata.itens = addresses;
+            itemdata.mark = "addressmark";
+            
+            for (ExtendedObject oaddress : oaddresses) {
+                itemdata.object = oaddress;
+                Common.insertItem(itemdata);
+            }
+
+            addresses.setVisible(true);
+            editaddress.setVisible(true);
+            addaddress.setVisible(true);
+            removeaddress.setVisible(true);
+            
+            break;
+        }
+    }
+    
+    private static final void buildContactTab(TabbedPane tabs,
+            DocumentModel model, ViewData view) {
+        ItemData itemdata;
+        TabbedPaneItem tab;
+        DataItem dataitem;
+        Table contacts, addresses;
+        Button addcontact, removecontact, editcontact;
+        Container contactcnt = new StandardContainer(tabs, "contactscnt");
+        DataForm contact = new DataForm(contactcnt, "contact");
+        byte modo = Common.getMode(view);
         ExtendedObject[] ocontacts = view.getParameter("contacts");
         
-        /*
-         * Identity
-         */
-        partner = new DataForm(tabs, "identity");
-        partner.importModel(models[Common.IDENTITY]);
+        contact.importModel(model);
+        contact.get("CODIGO").setEnabled(false);
+        contact.get("PARTNER_ID").setVisible(false);
+        
+        dataitem = contact.get("ADDRESS");
+        dataitem.getModelItem().setReference(null);
+        dataitem.setComponentType(Const.LIST_BOX);
+//
+//        contactCommunication = new StandardContainer(
+//                contactcnt, "contact.communic");
+        
+        editcontact = new Button(contactcnt, "editcontact");
+        addcontact = new Button(contactcnt, "addcontact");
+        removecontact = new Button(contactcnt, "removecontact");
+        
+        contacts = new Table(contactcnt, "contacts");
+        contacts.importModel(model);
+        contacts.getColumn("PARTNER_ID").setVisible(false);
+        contacts.setVisible(false);
+        contacts.setMark(true);
+
+        Common.enableTableColumns(contacts, "CODIGO", "PNOME", "UNOME");
+        
+        tab = new TabbedPaneItem(tabs, "contacttab");
+        tab.setContainer(contactcnt);
+        
+        switch (modo) {
+        case Common.CREATE:
+            editcontact.setVisible(false);
+            removecontact.setVisible(false);
+            
+            break;
+        
+        case Common.SHOW:
+            contacts.setMark(false);
+            
+            if (ocontacts != null) {
+                itemdata = new ItemData();
+                itemdata.container = contactcnt;
+                itemdata.itens = contacts;
+                itemdata.mark = "contactmark";
+                contacts.setVisible(true);
+                
+                for (ExtendedObject ocontact : ocontacts) {
+                    itemdata.object = ocontact;
+                    Common.insertItem(itemdata);
+                }
+                
+                dataitem = contact.get("ADDRESS");
+                addresses = view.getElement("addresses");
+                Common.loadListFromTable(dataitem, addresses, "LOGRADOURO",
+                        "CODIGO");
+            }
+            
+            editcontact.setVisible(false);
+            addcontact.setVisible(false);
+            removecontact.setVisible(false);
+            
+            break;
+            
+        case Common.UPDATE:
+            contacts.setMark(true);
+            
+            if (ocontacts != null) {
+                itemdata = new ItemData();
+                itemdata.container = contactcnt;
+                itemdata.itens = contacts;
+                itemdata.mark = "contactmark";
+                
+                for (ExtendedObject ocontact : ocontacts) {
+                    itemdata.object = ocontact;
+                    Common.insertItem(itemdata);
+                }
+                
+                dataitem = contact.get("ADDRESS");
+                addresses = view.getElement("addresses");
+                Common.loadListFromTable(dataitem, addresses, "LOGRADOURO",
+                        "CODIGO");
+                
+                contacts.setVisible(true);
+                editcontact.setVisible(true);
+                addcontact.setVisible(true);
+                removecontact.setVisible(true);
+            }
+            
+            break;
+        }
+    }
+    
+    /**
+     * 
+     * @param tabs
+     * @param model
+     * @param view
+     */
+    private static final void buildIdentityTab(TabbedPane tabs,
+            DocumentModel model, ViewData view) {
+        TabbedPaneItem tab;
+        DataItem dataitem;
+        String name;
+        DataForm partner = new DataForm(tabs, "identity");
+        ExtendedObject opartner = view.getParameter("partner");
+        byte modo = Common.getMode(view);
+        
+        partner.importModel(model);
         
         for (Element element : partner.getElements()) {
             if (!element.isDataStorable())
@@ -74,167 +247,51 @@ public class Response {
         tab = new TabbedPaneItem(tabs, "identitytab");
         tab.setContainer(partner);
         
-        save = new Button(container, "save");
+        switch (modo) {
+        case Common.UPDATE:
+        case Common.SHOW:
+            partner.setObject(opartner);
+            
+            break;
+        }
+    }
+    
+    /**
+     * 
+     * @param view
+     * @param identitymodel
+     * @param addressmodel
+     */
+    public static final void identity(ViewData view, DocumentModel[] models) {
+        Container container = new Form(view, "main");
+        TabbedPane tabs = new TabbedPane(container, "pane");
+        Button save = new Button(container, "save");
+        byte modo = Common.getMode(view);
+        
+        /*
+         * Identity
+         */
+        buildIdentityTab(tabs, models[Common.IDENTITY], view);
         
         /*
          * Address
          */
-        addresscnt = new StandardContainer(tabs, "addresscnt");
-        address = new DataForm(addresscnt, "address");
-        address.importModel(models[Common.ADDRESS]);
-        address.get("CODIGO").setEnabled(false);
-        address.get("PARTNER_ID").setVisible(false);
-        
-        addresses = new Table(addresscnt, "addresses");
-        addresses.importModel(models[Common.ADDRESS]);
-        addresses.setVisible(false);
-        addresses.setMark(true);
-        
-        Common.enableTableColumns(addresses,
-                "CODIGO", "TIPO_ENDERECO", "LOGRADOURO");
-        
-        editaddress = new Button(addresscnt, "editaddress");
-        addaddress = new Button(addresscnt, "addaddress");
-        removeaddress = new Button(addresscnt, "removeaddress");
-        
-        tab = new TabbedPaneItem(tabs, "addresstab");
-        tab.setContainer(addresscnt);
+        buildAddressTab(tabs, models[Common.ADDRESS], view);
         
         /*
          * Contacts
          */
-        contactcnt = new StandardContainer(tabs, "contactscnt");
-        contact = new DataForm(contactcnt, "contact");
-        contact.importModel(models[Common.CONTACT]);
-        contact.get("CODIGO").setEnabled(false);
-        contact.get("PARTNER_ID").setVisible(false);
-        
-        dataitem = contact.get("ADDRESS");
-        dataitem.getModelItem().setReference(null);
-        dataitem.setComponentType(Const.LIST_BOX);
-        
-        contacts = new Table(contactcnt, "contacts");
-        contacts.importModel(models[Common.CONTACT]);
-        contacts.getColumn("PARTNER_ID").setVisible(false);
-        contacts.setVisible(false);
-        contacts.setMark(true);
-
-        Common.enableTableColumns(contacts, "CODIGO", "PNOME", "UNOME");
-
-        editcontact = new Button(contactcnt, "editcontact");
-        addcontact = new Button(contactcnt, "addcontact");
-        removecontact = new Button(contactcnt, "removecontact");
-        
-        tab = new TabbedPaneItem(tabs, "contacttab");
-        tab.setContainer(contactcnt);
+        buildContactTab(tabs, models[Common.CONTACT], view);
         
         switch (modo) {
+        case Common.UPDATE:
         case Common.CREATE:
             save.setVisible(true);
-            editaddress.setVisible(false);
-            removeaddress.setVisible(false);
-            editcontact.setVisible(false);
-            removecontact.setVisible(false);
             
             break;
         
         case Common.SHOW:
-            addresses.setMark(false);
-            contacts.setMark(false);
-            
-            partner.setObject(opartner);
-
-            if (oaddresses != null) {
-                itemdata = new ItemData();
-                itemdata.container = addresscnt;
-                itemdata.itens = addresses;
-                itemdata.mark = "addressmark";
-                addresses.setVisible(true);
-                
-                for (ExtendedObject oaddress : oaddresses) {
-                    itemdata.object = oaddress;
-                    Common.insertItem(itemdata);
-                }
-                
-                dataitem = contact.get("ADDRESS");
-                Common.loadListFromTable(dataitem, addresses, "CODIGO",
-                        "LOGRADOURO");
-            } else {
-                itemdata = null;
-            }
-            
-            if (ocontacts != null) {
-                if (itemdata == null)
-                    itemdata = new ItemData();
-                
-                itemdata.container = contactcnt;
-                itemdata.itens = contacts;
-                itemdata.mark = "contactmark";
-                contacts.setVisible(true);
-                
-                for (ExtendedObject ocontact : ocontacts) {
-                    itemdata.object = ocontact;
-                    Common.insertItem(itemdata);
-                }
-            }
-            
             save.setVisible(false);
-            editaddress.setVisible(false);
-            addaddress.setVisible(false);
-            removeaddress.setVisible(false);
-            editcontact.setVisible(false);
-            addcontact.setVisible(false);
-            removecontact.setVisible(false);
-            
-            break;
-            
-        case Common.UPDATE:
-            addresses.setMark(true);
-            contacts.setMark(true);
-            
-            partner.setObject(opartner);
-            
-            if (oaddresses != null) {
-                itemdata = new ItemData();
-                itemdata.container = addresscnt;
-                itemdata.itens = addresses;
-                itemdata.mark = "addressmark";
-                
-                for (ExtendedObject oaddress : oaddresses) {
-                    itemdata.object = oaddress;
-                    Common.insertItem(itemdata);
-                }
-                
-                dataitem = contact.get("ADDRESS");
-                Common.loadListFromTable(dataitem, addresses, "LOGRADOURO",
-                        "CODIGO");
-                
-                addresses.setVisible(true);
-                editaddress.setVisible(true);
-                addaddress.setVisible(true);
-                removeaddress.setVisible(true);
-            } else {
-                itemdata = null;
-            }
-            
-            if (ocontacts != null) {
-                if (itemdata == null)
-                    itemdata = new ItemData();
-                
-                itemdata.container = contactcnt;
-                itemdata.itens = contacts;
-                itemdata.mark = "contactmark";
-                
-                for (ExtendedObject ocontact : ocontacts) {
-                    itemdata.object = ocontact;
-                    Common.insertItem(itemdata);
-                }
-                
-                contacts.setVisible(true);
-                editcontact.setVisible(true);
-                addcontact.setVisible(true);
-                removecontact.setVisible(true);
-            }
             
             break;
         }
