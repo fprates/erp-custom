a32c2743c403599db7c7e664f8763b6553fae51d
Francisco Prates <mad.kanie@gmail.com>
2012-06-04 17:07:27 -0300

    scripts: implementa script para inicializaçao de tabelas.
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

---
+++ scripts/db-install-00-erp-custom.sql
@@ -0,0 +1,13 @@
+drop table csddocitm if exists;
+drop table csddochdr if exists;
+drop table CSUBMAT if exists;
+drop table cmatprice if exists;
+drop table cmatpromo if exists;
+drop table cmaterial if exists;
+drop table cpartnercomm if exists;
+drop table communic if exists;
+drop table cprtnrcntct if exists;
+drop table cpartneraddr if exists;
+drop table caddrtype if exists;
+drop table cpartner if exists;
+drop table cprtnrtype if exists;
