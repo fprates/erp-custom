c4865752c7753e32112afa68b188deaf73c9b889
Francisco Prates <mad.kanie@gmail.com>
2014-02-15 17:20:37 -0200

    sd: corrige referencia em regioes.
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- src/sd/partner/classes/org/erp/custom/sd/partner/install/MasterData.java
+++ src/sd/partner/classes/org/erp/custom/sd/partner/install/MasterData.java
@@ -131,32 +131,32 @@ public class MasterData {
         model.add(item);
         
         context.data.addValues(model, "BRAC", "BR", "AC", "Acre");
-        context.data.addValues(model, "BRAL", "AL", "BR", "Alagoas");
-        context.data.addValues(model, "BRAM", "AM", "BR", "Amazonas");
-        context.data.addValues(model, "BRAP", "AP", "BR", "Amapá");
-        context.data.addValues(model, "BRBA", "BA", "BR", "Bahia");
-        context.data.addValues(model, "BRCE", "CE", "BR", "Ceará");
-        context.data.addValues(model, "BRDF", "DF", "BR", "Brasília");
-        context.data.addValues(model, "BRES", "ES", "BR", "Espírito Santo");
-        context.data.addValues(model, "BRGO", "GO", "BR", "Goiás");
-        context.data.addValues(model, "BRMA", "MA", "BR", "Maranhão");
-        context.data.addValues(model, "BRMG", "MG", "BR", "Minas Gerais");
-        context.data.addValues(model, "BRMS", "MS", "BR", "Mato Grosso do Sul");
-        context.data.addValues(model, "BRMT", "MT", "BR", "Mato Grosso");
-        context.data.addValues(model, "BRPA", "PA", "BR", "Pará");
-        context.data.addValues(model, "BRPB", "PB", "BR", "Paraíba");
-        context.data.addValues(model, "BRPE", "PE", "BR", "Pernambuco");
-        context.data.addValues(model, "BRPI", "PI", "BR", "Piauí");
-        context.data.addValues(model, "BRPR", "PR", "BR", "Paraná");
-        context.data.addValues(model, "BRRJ", "RJ", "BR", "Rio de Janeiro");
-        context.data.addValues(model, "BRRN", "RN", "BR", "Rio Grande do "
+        context.data.addValues(model, "BRAL", "BR", "AL", "Alagoas");
+        context.data.addValues(model, "BRAM", "BR", "AM", "Amazonas");
+        context.data.addValues(model, "BRAP", "BR", "AP", "Amapá");
+        context.data.addValues(model, "BRBA", "BR", "BA", "Bahia");
+        context.data.addValues(model, "BRCE", "BR", "CE", "Ceará");
+        context.data.addValues(model, "BRDF", "BR", "DF", "Brasília");
+        context.data.addValues(model, "BRES", "BR", "ES", "Espírito Santo");
+        context.data.addValues(model, "BRGO", "BR", "GO", "Goiás");
+        context.data.addValues(model, "BRMA", "BR", "MA", "Maranhão");
+        context.data.addValues(model, "BRMG", "BR", "MG", "Minas Gerais");
+        context.data.addValues(model, "BRMS", "BR", "MS", "Mato Grosso do Sul");
+        context.data.addValues(model, "BRMT", "BR", "MT", "Mato Grosso");
+        context.data.addValues(model, "BRPA", "BR", "PA", "Pará");
+        context.data.addValues(model, "BRPB", "BR", "PB", "Paraíba");
+        context.data.addValues(model, "BRPE", "BR", "PE", "Pernambuco");
+        context.data.addValues(model, "BRPI", "BR", "PI", "Piauí");
+        context.data.addValues(model, "BRPR", "BR", "PR", "Paraná");
+        context.data.addValues(model, "BRRJ", "BR", "RJ", "Rio de Janeiro");
+        context.data.addValues(model, "BRRN", "BR", "RN", "Rio Grande do "
                 + "Norte");
-        context.data.addValues(model, "BRRO", "RO", "BR", "Rondônia");
-        context.data.addValues(model, "BRRR", "RR", "BR", "Roraima");
-        context.data.addValues(model, "BRRS", "RS", "BR", "Rio Grande do Sul");
-        context.data.addValues(model, "BRSC", "SC", "BR", "Santa Catarina");
-        context.data.addValues(model, "BRSE", "SE", "BR", "Sergipe");
-        context.data.addValues(model, "BRSP", "SP", "BR", "São Paulo");
-        context.data.addValues(model, "BRTO", "TO", "BR", "Tocantins");
+        context.data.addValues(model, "BRRO", "BR", "RO", "Rondônia");
+        context.data.addValues(model, "BRRR", "BR", "RR", "Roraima");
+        context.data.addValues(model, "BRRS", "BR", "RS", "Rio Grande do Sul");
+        context.data.addValues(model, "BRSC", "BR", "SC", "Santa Catarina");
+        context.data.addValues(model, "BRSE", "BR", "SE", "Sergipe");
+        context.data.addValues(model, "BRSP", "BR", "SP", "São Paulo");
+        context.data.addValues(model, "BRTO", "BR", "TO", "Tocantins");
     }
 }
